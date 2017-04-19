588443f78342cc3fb9d511d455150e3d6fe58749
d.efimov <d.efimov@fun-box.ru>
2017-04-19 16:21:01 +0400

    cover tomogram action

---
+++ spec/lib/tomograph/tomogram/action_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'tomograph/tomogram/action'
+
+RSpec.describe Tomograph::Tomogram::Action do
+  describe '#find_responses' do
+    let(:status) {''}
+    let(:responses) {[{'status' => status}]}
+
+    subject do
+      described_class.new(path: '', method: '', request: '', responses: responses)
+    end
+
+    it 'returns responses' do
+      expect(subject.find_responses(status: status)).to eq(responses)
+    end
+  end
+end
