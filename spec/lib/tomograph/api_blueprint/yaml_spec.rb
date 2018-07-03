54fd4c99a3bce272e3eb53e394e355be00d5285e
n.epifanov <n.epifanov@fun-box.ru>
2018-07-03 14:17:35 +0400

    Add tests for Tomograph::ApiBlueprint::Yaml

--- spec/lib/tomograph/api_blueprint/yaml_spec.rb
+++ spec/lib/tomograph/api_blueprint/yaml_spec.rb
@@ -7,4 +7,71 @@ RSpec.describe Tomograph::ApiBlueprint::Yaml do
     allow_any_instance_of(YAML).to receive(:safe_load).and_return(double)
     expect { described_class.new(nil, '', nil) }.not_to raise_exception
   end
+
+  context 'given YAML with two actions' do
+    subject { described_class.new('', nil, input_file) }
+    let(:input_file) { 'spec/fixtures/api3.yaml' }
+    let(:tomogram_hash) do
+      [{"path"=>"/sessions",
+        "method"=>"POST",
+        "content-type"=>"application/json",
+        "request"=>
+        {"$schema"=>"http://json-schema.org/draft-04/schema#",
+         "type"=>"object",
+         "properties"=>
+         {"login"=>{"type"=>"string"},
+          "password"=>{"type"=>"string"},
+          "captcha"=>{"type"=>"string"}},
+         "required"=>["login", "password"]},
+        "responses"=>
+        [{"status"=>"401", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"429", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"201",
+          "body"=>
+          {"$schema"=>"http://json-schema.org/draft-04/schema#",
+           "type"=>"object",
+           "properties"=>
+           {"confirmation"=>
+            {"type"=>"object",
+             "properties"=>
+             {"id"=>{"type"=>"string"},
+              "type"=>{"type"=>"string"},
+              "operation"=>{"type"=>"string"}},
+             "required"=>["id", "type", "operation"]},
+            "captcha"=>{"type"=>"string"},
+            "captcha_does_not_match"=>{"type"=>"boolean"}}},
+          "content-type"=>"application/json"}],
+        "resource"=>"/sessions"},
+       {"path"=>"/sessions/{id}",
+        "method"=>"DELETE",
+        "content-type"=>"application/json",
+        "request"=>
+        {"$schema"=>"http://json-schema.org/draft-04/schema#",
+         "type"=>"object",
+         "properties"=>{"status"=>{"type"=>"string"}},
+         "required"=>["status"]},
+        "responses"=>
+        [{"status"=>"401", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"423", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"200",
+          "body"=>
+          {"$schema"=>"http://json-schema.org/draft-04/schema#",
+           "type"=>"object",
+           "properties"=>{"status"=>{"type"=>"string"}},
+           "required"=>["status"]},
+          "content-type"=>"application/json"}],
+        "resource"=>"/sessions"}]
+    end
+    let(:resource_map) do
+      {"/sessions"=>["POST /sessions", "DELETE /sessions/{id}"]}
+    end
+
+    it 'produces correct Tomogram' do
+      expect(subject.to_tomogram.map(&:to_hash)).to eq(tomogram_hash)
+    end
+
+    it 'produces correct resource map' do
+      expect(subject.to_resources).to eq(resource_map)
+    end
+  end
 end
