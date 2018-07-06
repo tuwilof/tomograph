ddbee5ca0a784336a53a51420f7747051b72946b
n.epifanov <n.epifanov@fun-box.ru>
2018-07-06 13:22:07 +0400

    Fix tests

--- spec/lib/tomograph/api_blueprint/json_schema_spec.rb
+++ spec/lib/tomograph/api_blueprint/json_schema_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Tomograph::ApiBlueprint::JsonSchema do
     subject { described_class.new('', input_file) }
     let(:input_file) { 'spec/fixtures/api3.json' }
     let(:tomogram_hash) do
-      [{"path"=>"/sessions",
+      [{"path"=>Tomograph::Path.new("/sessions"),
         "method"=>"POST",
         "content-type"=>"application/json",
         "request"=>
@@ -36,7 +36,7 @@ RSpec.describe Tomograph::ApiBlueprint::JsonSchema do
             "captcha_does_not_match"=>{"type"=>"boolean"}}},
           "content-type"=>"application/json"}],
         "resource"=>"/sessions"},
-       {"path"=>"/sessions/{id}",
+       {"path"=>Tomograph::Path.new("/sessions/{id}"),
         "method"=>"DELETE",
         "content-type"=>"application/json",
         "request"=>
