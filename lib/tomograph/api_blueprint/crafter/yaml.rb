1c389e8f8706471f79f5b3a8d6c47d05ae95a166
d.efimov <d.efimov@fun-box.ru>
2019-05-29 18:28:13 +0400

    add two tests for crafter

---
+++ lib/tomograph/api_blueprint/crafter/yaml.rb
@@ -0,0 +1,130 @@
+require 'yaml'
+require 'tomograph/api_blueprint/crafter/yaml/action'
+require 'tomograph/tomogram/action'
+
+module Tomograph
+  module ApiBlueprint
+    class Crafter
+      class Yaml
+        def initialize(prefix, apib_path, drafter_yaml_path)
+          @prefix = prefix
+          @documentation = if apib_path
+                             YAML.safe_load(`drafter #{apib_path}`)
+                           elsif drafter_yaml_path
+                             YAML.safe_load(File.read(drafter_yaml_path))
+                           end
+        end
+
+        def groups
+          @groups ||= @documentation['content'][0]['content'].each_with_object([]) do |group, result_groups|
+            result_groups.push(group) if group?(group)
+            result_groups.push('content' => [group]) if single_resource?(group)
+          end
+        end
+
+        def single_resource?(group)
+          group['element'] == 'resource'
+        end
+
+        def group?(group)
+          return false if group['element'] == 'resource'
+          group['element'] != 'copy' && # Element is a human readable text
+            group['meta']['classes']['content'][0]['content'] == 'resourceGroup' # skip Data Structures
+        end
+
+        def resources
+          @resources ||= groups.inject([]) do |result_groups, group|
+            result_groups.push(group['content'].each_with_object([]) do |resource, result_resources|
+              result_resources.push('resource' => resource, 'resource_path' => resource_path(resource)) if resource?(resource)
+            end)
+          end.flatten
+        end
+
+        def resource?(resource)
+          resource['element'] != 'copy' # Element is a human readable text
+        end
+
+        def resource_path(resource)
+          resource['attributes'] && resource['attributes']['href']['content']
+        end
+
+        def transitions
+          @transitions ||= resources.inject([]) do |result_resources, resource|
+            result_resources.push(resource['resource']['content'].each_with_object([]) do |transition, result_transitions|
+              result_transitions.push(transition_hash(transition, resource)) if transition?(transition)
+            end)
+          end.flatten
+        end
+
+        def transition?(transition)
+          transition['element'] == 'transition'
+        end
+
+        def transition_hash(transition, resource)
+          {
+            'transition' => transition,
+            'transition_path' => transition_path(transition, resource['resource_path']),
+            'resource' => resource['resource_path']
+          }
+        end
+
+        def transition_path(transition, resource_path)
+          transition['attributes'] && transition['attributes']['href']['content'] || resource_path
+        end
+
+        def without_group_actions
+          transitions.inject([]) do |result_transition, transition|
+            result_transition.push(transition['transition']['content'].each_with_object([]) do |content, result_contents|
+              result_contents.push(Tomograph::ApiBlueprint::Crafter::Yaml::Action.new(
+                                     content['content'],
+                                     transition['transition_path'],
+                                     transition['resource']
+              )) if action?(content)
+            end)
+          end
+        end
+
+        def action?(content)
+          content['element'] == 'httpTransaction'
+        end
+
+        def actions
+          @actions ||= without_group_actions
+                       .flatten
+                       .group_by { |action| "#{action.method} #{action.path}" }.map do |_key, related_actions|
+            action_hash(related_actions)
+          end.flatten
+        end
+
+        def action_hash(related_actions)
+          {
+            path: "#{@prefix}#{related_actions.first.path}",
+            method: related_actions.first.method,
+            content_type: related_actions.first.content_type,
+            request: related_actions.first.request,
+            responses: related_actions.map(&:responses).flatten,
+            resource: related_actions.first.resource
+          }
+        end
+
+        def to_tomogram
+          @tomogram ||= actions.inject([]) do |result, action|
+            result.push(Tomograph::Tomogram::Action.new(action))
+          end
+        end
+
+        def to_resources
+          return @to_resources if @to_resources
+
+          @to_resources = actions.group_by { |action| action[:resource] }
+          @to_resources = @to_resources.inject({}) do |res, related_actions|
+            requests = related_actions[1].map do |action|
+              "#{action[:method]} #{action[:path]}"
+            end
+            res.merge(related_actions[1].first[:resource] => requests)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/tomograph/api_blueprint/crafter/yaml_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+require 'tomograph/api_blueprint/crafter/yaml'
+
+RSpec.describe Tomograph::ApiBlueprint::Crafter::Yaml do
+  it 'not raise exception' do
+    allow_any_instance_of(Kernel).to receive(:`).and_return('')
+    allow_any_instance_of(YAML).to receive(:safe_load).and_return(double)
+    expect { described_class.new(nil, '', nil) }.not_to raise_exception
+  end
+
+  context 'given YAML with two actions' do
+    subject { described_class.new('/api/v1', nil, input_file) }
+    let(:input_file) { 'spec/fixtures/crafter/api3.yaml' }
+    let(:tomogram_hash) do
+      [{"path"=>Tomograph::Path.new("/api/v1/sessions"),
+        "method"=>"POST",
+        "content-type"=>"application/json",
+        "request"=>
+        {"$schema"=>"http://json-schema.org/draft-04/schema#",
+         "type"=>"object",
+         "properties"=>
+         {"login"=>{"type"=>"string"},
+          "password"=>{"type"=>"string"},
+          "captcha"=>{"type"=>"string"}},
+         "required"=>["login", "password"]},
+        "responses"=>
+        [{"status"=>"401", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"429", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"201",
+          "body"=>
+          {"$schema"=>"http://json-schema.org/draft-04/schema#",
+           "type"=>"object",
+           "properties"=>
+           {"confirmation"=>
+            {"type"=>"object",
+             "properties"=>
+             {"id"=>{"type"=>"string"},
+              "type"=>{"type"=>"string"},
+              "operation"=>{"type"=>"string"}},
+             "required"=>["id", "type", "operation"]},
+            "captcha"=>{"type"=>"string"},
+            "captcha_does_not_match"=>{"type"=>"boolean"}}},
+          "content-type"=>"application/json"}],
+        "resource"=>"/sessions"},
+       {"path"=>Tomograph::Path.new("/api/v1/sessions/{id}"),
+        "method"=>"DELETE",
+        "content-type"=>"application/json",
+        "request"=>
+        {"$schema"=>"http://json-schema.org/draft-04/schema#",
+         "type"=>"object",
+         "properties"=>{"status"=>{"type"=>"string"}},
+         "required"=>["status"]},
+        "responses"=>
+        [{"status"=>"401", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"423", "body"=>{}, "content-type"=>"application/json"},
+         {"status"=>"200",
+          "body"=>
+          {"$schema"=>"http://json-schema.org/draft-04/schema#",
+           "type"=>"object",
+           "properties"=>{"status"=>{"type"=>"string"}},
+           "required"=>["status"]},
+          "content-type"=>"application/json"}],
+        "resource"=>"/sessions"}]
+    end
+    let(:resource_map) do
+      {"/sessions"=>["POST /api/v1/sessions", "DELETE /api/v1/sessions/{id}"]}
+    end
+
+    it 'produces correct Tomogram' do
+      skip
+      expect(subject.to_tomogram.map(&:to_hash)).to eq(tomogram_hash)
+    end
+
+    it 'produces correct resource map' do
+      skip
+      expect(subject.to_resources).to eq(resource_map)
+    end
+  end
+end
