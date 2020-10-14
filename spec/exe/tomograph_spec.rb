a1b8e530da3be93c10b82aecbc06b061fd3ca8d3
Dmitry Efimov <tuwilof@gmail.com>
2020-10-14 16:00:58 +0400

    Revert "Delete methadone, use rake task instead"

--- spec/exe/tomograph_spec.rb
+++ spec/exe/tomograph_spec.rb
@@ -1,36 +1,25 @@
-require 'rake'
 require 'tempfile'
 require 'spec_helper'
 
-describe 'tomograph', type: :task do
+RSpec.describe 'tomograph' do
   let(:input_path) { 'spec/fixtures/drafter_4/exe.yaml' }
   let(:options) {}
   let(:output_file) { Tempfile.new('') }
   let(:output_path) { output_file.path }
 
-  subject(:rake_task) { Rake.application[:tomograph] }
-
-  before(:all) do
-    Rake.application.init
-    Rake.application.load_rakefile
+  subject(:run_tomograph) do
+    system("bundle exec ./exe/tomograph #{options} '#{input_path}' '#{output_path}'")
   end
-
-  before { rake_task.reenable }
 
   def run_tests_with_file
     Tempfile.open('') do |output|
-      expect(rake_task.invoke).to be_truthy
+      expect(run_tomograph).to be_truthy
       expect(IO.read(output_path)).to eq(IO.read(expected_path))
     end
   end
 
   context 'in default mode' do
     let(:expected_path) { 'spec/fixtures/tomogram/exe.json' }
-
-    before do
-      argv = ["tomograph", "--", "--input=#{input_path}", "--output=#{output_path}"]
-      stub_const("ARGV", argv)
-    end
 
     it 'produces correct json' do
       run_tests_with_file
@@ -40,11 +29,6 @@ describe 'tomograph', type: :task do
   context 'with --exclude-description' do
     let(:expected_path) { 'spec/fixtures/tomogram/exe-exclude-description.json' }
     let(:options) { '--exclude-description' }
-
-    before do
-      argv = ["tomograph", "--", options, "--input=#{input_path}", "--output=#{output_path}"]
-      stub_const("ARGV", argv)
-    end
 
     it 'produces correct json' do
       run_tests_with_file
@@ -62,13 +46,8 @@ describe 'tomograph', type: :task do
     let(:sessions_DELETE_path) { 'spec/fixtures/tomogram/exe-split-#sessions#(id) DELETE.json' }
     let(:output_path) { Dir.mktmpdir }
 
-    before do
-      argv = ["tomograph", "--", options, "--input=#{input_path}", "--output=#{output_path}"]
-      stub_const("ARGV", argv)
-    end
-
     it 'produces correct json' do
-      expect(rake_task.invoke(options, input_path, output_path)).to be_truthy
+      expect(run_tomograph).to be_truthy
       expect(json_read(output_path, '#sessions POST.json')).to eq(json_read(sessions_POST_path))
       expect(json_read(output_path, '#sessions#(id) DELETE.json')).to eq(json_read(sessions_DELETE_path))
       expect(Dir.entries(output_path).count).to eq(4)
